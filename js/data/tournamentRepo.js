2dcc2ec7755bbe23ec484308fdffc569f991d97a
JeremiasReinoso <141459619+JeremiasReinoso@users.noreply.github.com>
2026-09-14 15:08:05 -0300

    Update tournamentRepo.js

--- js/data/tournamentRepo.js
+++ js/data/tournamentRepo.js
@@ -1,25 +1,12 @@
-import { supabaseFetch } from './db.js';
 import { LicenciaRepo } from './licenseRepo.js';
+import { DataManager } from './dataManager.js';
 
 export const TorneoRepo = {
     obtenerTodos: async () => {
-        const licencia = await LicenciaRepo.obtenerActiva();
-        if (!licencia) throw new Error('Activá una licencia válida para consultar torneos.');
-        const url = `torneos?licencia_id=eq.${encodeURIComponent(licencia.id)}&select=*`;
-        return await supabaseFetch(url);
+        return DataManager.getTournaments();
     },
     crear: async (nombre, partidosAsegurados) => {
-        const licencia = await LicenciaRepo.obtenerActiva();
-        if (!licencia) throw new Error('Activá una licencia válida para crear torneos.');
-
-        const nuevoTorneo = {
-            licencia_id: licencia.id,
-            nombre,
-            partidos_asegurados: partidosAsegurados,
-            estado: 'activo'
-        };
-
         await LicenciaRepo.consumirTorneo();
-        await supabaseFetch('torneos', 'POST', nuevoTorneo);
+        return DataManager.createTournament(nombre, partidosAsegurados);
     }
 };
